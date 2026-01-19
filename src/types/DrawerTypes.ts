0ca0797433ca47beb42f9e753bb4331043a16925
Villiamremote <willyp@hotmail.se>
2026-01-19 16:25:46 +0100

    added some conditinal styling

--- src/types/DrawerTypes.ts
+++ src/types/DrawerTypes.ts
@@ -1,4 +1,4 @@
-type ThemeId = "light" | "dark" | "snow" | "beige" | "forest";
+type ThemeId = "light" | "dark" | "snow" | "beige" | "forest" | "midnight";
 export type {ThemeId}
 
 export type CookiePrefs = {
