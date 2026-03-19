40bb711eaa7ca534953c77344011295dece3a8df
Villiamremote <willyp@hotmail.se>
2026-03-19 11:32:48 +0100

    updated the admin nav-bar

---
+++ src/components/Admin/AdminHeader.tsx
@@ -0,0 +1,36 @@
+import { useLogout } from "@/hooks/Auth/useAuth";
+import { NavLink, useNavigate } from "react-router-dom";
+
+const AdminHeader = () => {
+      const logout = useLogout();
+      const nav = useNavigate();
+    
+      const handleLogout = async () => {
+        try {
+          await logout.mutateAsync();
+          nav("/", {replace: true});
+        } catch{}
+      }
+  return (
+    <header className="admin-header">            
+
+    <nav className="admin-nav-group">
+        <NavLink to="/" className="admin-nav">Back to start</NavLink>
+        <NavLink to="/admin" className="admin-nav" end>Dashbord</NavLink>
+        <NavLink to="adminproducts" className="admin-nav">Products</NavLink>
+        <NavLink to="admin-orders" className="admin-nav">Orders</NavLink>
+    </nav>
+
+
+        <button
+        type="button"
+        onClick={handleLogout}
+        className="btn btn-primary"
+        >
+        Logga ut
+        </button>
+    </header>
+  );
+};
+
+export default AdminHeader;
